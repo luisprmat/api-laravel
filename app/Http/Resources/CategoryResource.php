8b1633c190da294d6e0d0f3466b350049d50bbcb
Luis Parrado <luisprmat@gmail.com>
2024-01-20 21:23:11 -0500

    Different attribute when route is list or single.

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -17,7 +17,11 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'description' => $this->when($request->is('api/categories*'), function () {
+            'description' => $this->when($request->is('api/categories*'), function () use ($request) {
+                if ($request->is('api/categories')) {
+                    return str($this->description)->limit(20);
+                };
+
                 return $this->description;
             }),
         ];
