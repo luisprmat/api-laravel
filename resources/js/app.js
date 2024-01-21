5accbf1e59b167e95727f3ca97975b4c57d01480
Luis Parrado <luisprmat@gmail.com>
2024-01-21 13:02:26 -0500

    Install and configure vue + first component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp } from 'vue';
+import Home from '@/components/Home.vue';
+
+createApp({})
+    .component('Home', Home)
+    .mount('#app')
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    New component
+</template>
