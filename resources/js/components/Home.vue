1c5b96d4e22215dc6015047fe134b911e282f1d2
Luis Parrado <luisprmat@gmail.com>
2024-01-21 20:40:06 -0500

    Web Client with Vue.js, get categories and products

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,3 +1,31 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const categories = ref({})
+const products = ref({})
+
+const getCategories = async () => {
+    await axios.get('/api/categories')
+        .then(response => {
+            categories.value = response.data.data
+        })
+        .catch((error) => console.log(error))
+}
+
+const getProducts = async () => {
+    await axios.get('/api/products')
+        .then(response => {
+            products.value = response.data.data
+        })
+        .catch((error) => console.log(error))
+}
+
+onMounted(() => {
+    getCategories()
+    getProducts()
+})
+</script>
+
 <template>
     New component
 </template>
