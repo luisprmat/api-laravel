35b0dd79786dfd9b8c3c09171dc6c3db8b624eed
Luis Parrado <luisprmat@gmail.com>
2024-01-24 15:30:39 -0500

    Fix active route classes - Use custom RouterLink

---
+++ resources/js/Components/AppLink.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+    activeClasses: {
+        type: String
+    },
+    inactiveClasses: {
+        type: String
+    }
+})
+</script>
+
+<template>
+    <RouterLink v-bind="$props" custom v-slot="{ isActive, href, navigate }">
+        <a v-bind="$attrs" :href="href" @click="navigate" :class="isActive ? activeClasses : inactiveClasses">
+            <slot />
+        </a>
+    </RouterLink>
+</template>
--- resources/js/Components/ResponsiveNavLink.vue
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { computed } from 'vue'
-import { RouterLink } from 'vue-router'
+import AppLink from '@/Components/AppLink.vue'
 
 const props = defineProps({
     href: {
@@ -10,16 +9,12 @@ const props = defineProps({
         type: Boolean
     }
 })
-
-const classes = computed(() =>
-    props.active
-        ? 'block w-full pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-left text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out'
-)
 </script>
 
 <template>
-    <RouterLink :href="href" :class="classes">
+    <AppLink v-bind="$attrs"
+        active-classes="block w-full pl-3 pr-4 py-2 border-l-4 border-indigo-400 text-left text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out"
+        inactive-classes="block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">
         <slot />
-    </RouterLink>
+    </AppLink>
 </template>
