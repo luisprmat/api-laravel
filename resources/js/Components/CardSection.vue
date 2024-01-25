2cdadc9879348b2a14872fa90c079e71dd096b9e
Luis Parrado <luisprmat@gmail.com>
2024-01-25 01:40:20 -0500

    Refactor in Sections; avoid repeated code with component, slots, props, computed, binding

---
+++ resources/js/Components/CardSection.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    withoutPadding: {
+        type: Boolean,
+    },
+    innerClasses: {
+        type: String,
+    }
+})
+
+const classes = computed(() =>
+    props.withoutPadding
+        ? 'text-gray-900 dark:text-gray-100'
+        : 'p-6 text-gray-900 dark:text-gray-100'
+)
+</script>
+
+<template>
+    <div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div :class="[classes, innerClasses]">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
